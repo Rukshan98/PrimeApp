e9ca28203e6eb0bb8b8c2ed8768707c8309226fa
RukshanPelicanCube <rukshan@pelicancube.com>
2023-07-22 15:49:43 +0400

    added navigation

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+export default function App() {
+
+
+    const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+        <Stack.Navigator>
+        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+      </Stack.Navigator>
+        
+        
+        </NavigationContainer>
+  );
+}
